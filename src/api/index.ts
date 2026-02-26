6f4f7ea2c82c16db51e4a00bae9d3b0379e7810d
Md-Huzaifa-Islam <huzaifaswe@gmail.com>
2026-02-26 13:20:59 +0600

    remove better auth used normal auth and readme

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,11 @@
 import { Router, type Router as RouterType } from "express";
-import { AdminRoutes } from "./module/admin/admin.route";
 import { AuthRoutes } from "./module/auth/auth.route";
 import { BookingsRoutes } from "./module/bookings/bookings.route";
 import { CategoriesRoutes } from "./module/categories/categories.route";
 import { ReviewsRoutes } from "./module/reviews/reviews.route";
-import { TutorRoutes } from "./module/tutor/tutor.route";
 import { TutorsRoutes } from "./module/tutors/tutors.route";
 
 const router: RouterType = Router();
-
-// admin route
-router.use("/admin", AdminRoutes);
 
 // auth route
 router.use("/auth", AuthRoutes);
@@ -23,9 +18,6 @@ router.use("/categories", CategoriesRoutes);
 
 // reviews route
 router.use("/reviews", ReviewsRoutes);
-
-// tutor route
-router.use("/tutor", TutorRoutes);
 
 // tutors route
 router.use("/tutors", TutorsRoutes);
